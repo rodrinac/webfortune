ea322d2db1d671af128ded7b0afb0a6cb5196399
josé Rodrigo da Silva Inácio <jose.inacio@cwi.com.br>
2025-05-28 23:36:08 +0100

    Fixes the fortune categories endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use futures::future::ok;
 use once_cell::sync::Lazy;
 use std::collections::HashSet;
 use std::env;
-use std::fs;
 use tokio::net::TcpListener;
 
 use hyper::body::Incoming;
@@ -21,57 +19,77 @@ struct CategoriesResponse {
     data: HashSet<String>,
 }
 
-fn get_fortune_files() -> HashSet<String> {
-    fs::read_dir("/usr/share/fortune")
-        .unwrap_or_else(|err| panic!("Failed to read fortune files: {}", err))
-        .filter_map(Result::ok)
-        .map(|entry| entry.file_name().to_string_lossy().into_owned())
-        .filter(|file| !file.contains('.'))
-        .collect()
+fn get_fortune_files() -> Result<HashSet<String>, String> {
+    Command::new("fortune")
+        .args(["-f"])
+        .output()
+        .map_err(|_| "Failed to load fortune!".to_string())
+        .and_then(|output: Output| {
+            String::from_utf8(output.stderr).map_err(|_| "Failed to parse fortune!".to_string())
+        })
+        .map(|stdout| {
+            stdout
+                .lines()
+                .enumerate()
+                .filter_map(|(i, line)| {
+                    if i > 0 {
+                        line.split_whitespace().last().map(|s| s.to_string())
+                    } else {
+                        None
+                    }
+                })
+                .collect::<HashSet<String>>()
+        })
 }
 
 fn get_fortune(category: String) -> Result<String, String> {
     Command::new("fortune")
         .args(["-a", category.as_str()])
         .output()
-        .map_err(|_| "Fail to load fortune".to_string())
+        .map_err(|_| "Failed to load fortune!".to_string())
         .and_then(|output: Output| {
-            String::from_utf8(output.stdout).map_err(|_| "Fail to parse fortune".to_string())
+            String::from_utf8(output.stdout).map_err(|_| "Failed to parse fortune!".to_string())
         })
 }
 
-static FORTUNE_FILES: Lazy<HashSet<String>> = Lazy::new(get_fortune_files);
+static FORTUNE_FILES: Lazy<HashSet<String>> = Lazy::new(|| {
+    get_fortune_files().unwrap_or_else(|err| {
+        eprintln!("Error initializing FORTUNE_FILES: {err}");
+        HashSet::new()
+    })
+});
 
 async fn handle_request(req: Request<Incoming>) -> Result<Response<String>, hyper::http::Error> {
     let path = req.uri().path();
 
     if path == "/categories" {
         let response = CategoriesResponse {
-            data: FORTUNE_FILES.to_owned(),
+            data: FORTUNE_FILES.clone(), // <- clone that HashSet
         };
-        let body = serde_json::to_string(&response).unwrap_or("[]".to_string());
+        let body = serde_json::to_string(&response).unwrap_or_else(|_| "[]".to_string());
 
-        return ok(Response::new(body)).await;
+        return Ok(Response::new(body + "\n"));
     }
 
     // path == "/"
     let category = req
         .uri()
         .query()
-        .map(|query| query.replace("category=", "").trim().to_owned())
+        .and_then(|q| q.strip_prefix("category="))
+        .map(|s| s.trim().to_string())
         .unwrap_or_default();
 
     let result = get_fortune(category);
 
     let fortune = match result {
-        Ok(result) => Response::new(result),
-        Err(error) => Response::builder()
+        Ok(text) => Response::new(text),
+        Err(err) => Response::builder()
             .status(StatusCode::NOT_FOUND)
-            .body(error)
+            .body(err)
             .unwrap(),
     };
 
-    ok(fortune).await
+    Ok(fortune)
 }
 
 fn get_host_and_port() -> (IpAddr, u16) {
@@ -93,7 +111,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     loop {
         let (stream, _) = listener.accept().await?;
-
         let io = TokioIo::new(stream);
 
         tokio::task::spawn(async move {
